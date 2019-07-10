1b0a744b8e5d69d3f4d54d479ba00b9001af83c0
nhinguyen99 <nhinguyenkorean@gmail.com>
2019-07-10 11:02:11 +0700

    Initial commit

---
+++ src/main/java/Sum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        double a,b;
+        Scanner sc = new Scanner((System.in));
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        System.out.println(a+b);
+    }
+}
